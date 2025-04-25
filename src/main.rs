26caa14cb11e040c12700bca8fa0cf0a45121241
GlennFolker <glenn.folk08@gmail.com>
2025-04-25 17:03:56 +0700

    0.16

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup, startup).run();
+}
+
+fn startup(mut exit: EventWriter<AppExit>) {
+    println!("Hello, Bevy + Cranelift!");
+    exit.write(AppExit::Success);
 }
